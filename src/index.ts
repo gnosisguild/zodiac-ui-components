edb257cb80f8ae6e326a6f1a812db3c06b4a1dff
Asgeir <asgeir@cluda.com>
2022-03-25 09:37:29 +0100

    Make the button available via the NPM package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import TextField from "./components/input/TextField"
 import Checkbox from "./components/input/Checkbox"
 import Theme from "./Theme"
+import Button from "./components/Button"
 
-export { Theme, TextField, Checkbox }
+export { Theme, TextField, Checkbox, Button }
