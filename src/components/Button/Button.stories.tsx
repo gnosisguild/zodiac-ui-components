7824f68f50446a9ee6fa1d2f3e1a3ec3e6508516
Colin Spence <udappia@gmail.com>
2022-03-29 11:52:39 -0600

    Adds Button via mui instead of custom component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import { Meta } from "@storybook/react/types-6-0"
 import { Story } from "@storybook/react"
-import Button, { ButtonProps } from "./Button"
-import { CircularProgress } from "@material-ui/core"
+import { Button, ButtonProps, CircularProgress } from "@material-ui/core"
 import { Add, ArrowForward } from '@material-ui/icons'
 
 export default {
@@ -21,47 +20,47 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
 export const Contained = Template.bind({})
 Contained.args = {
-  label: "Contained Button",
+  children: "Contained Button",
 }
 
 export const Outlined = Template.bind({})
 Outlined.args = {
-  label: "Outlined Button",
+  children: "Outlined Button",
   variant: "outlined",
 }
 
 export const Large = Template.bind({})
 Large.args = {
-  label: "Large Button",
+  children: "Large Button",
   size: "large",
 }
 
 export const Medium = Template.bind({})
 Medium.args = {
-  label: "Medium Button",
+  children: "Medium Button",
 }
 
 export const Small = Template.bind({})
 Small.args = {
-  label: "Small Button",
+  children: "Small Button",
   size: "small",
 }
 
 export const StartIcon = Template.bind({})
 StartIcon.args = {
-  label: "Icon Button",
+  children: "Icon Button",
   startIcon: <Add/>,
 }
 
 export const EndIcon = Template.bind({})
 EndIcon.args = {
   endIcon: <ArrowForward/>,
-  label: "Icon Button",
+  children: "Icon Button",
 }
 
 export const Loading = Template.bind({})
 Loading.args = {
   disabled: true,
-  label: "Loading...",
+  children: "Loading..",
   startIcon: <CircularProgress color="inherit" size={18}/>,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import TextField from "./components/input/TextField"
 import Checkbox from "./components/input/Checkbox"
 import Theme from "./Theme"
-import Button from "./components/Button"
+import { Button } from "@material-ui/core/Button"
 import Modal from "./components/Modal"
 
 export { Theme, TextField, Checkbox, Button, Modal }
