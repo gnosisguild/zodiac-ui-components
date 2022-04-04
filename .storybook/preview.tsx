89c245e2a21aead2051e51eac9103f75b386f808
Asgeir <asgeir@cluda.com>
2022-04-04 13:35:19 +0200

    Fix preview + Convert to typescript

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { ThemeProvider } from "styled-components"
+import { CssBaseline, ThemeProvider as MUIThemeProvider } from "@material-ui/core"
+import  {zodiacMuiTheme} from "../src/index"
+import { theme as gnosisStyledComponentsTheme } from "@gnosis.pm/safe-react-components"
 
 import { addDecorator } from "@storybook/react"
-import { Theme } from "../src/index"
 
-addDecorator((story) => <Theme>{story()}</Theme>)
+addDecorator((story) => (
+  <MUIThemeProvider theme={zodiacMuiTheme}>
+    <ThemeProvider theme={gnosisStyledComponentsTheme}>
+      <CssBaseline />
+      {story()}
+    </ThemeProvider>
+  </MUIThemeProvider>
+)
+
+
 
 export const parameters = {
   layout: "centered",
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import zodiacTheme, { colors, doubleBorder } from "./theme/zodiacTheme"
-import { Button } from "@material-ui/core"
-import Checkbox from "./components/input/Checkbox"
-import Paper from "./components/Paper"
-import Modal from "./components/Modal"
-import TextField from "./components/input/TextField"
+import zodiacMuiTheme, { colors, doubleBorder } from "./theme/zodiacTheme"
+import ZodiacCheckbox from "./components/input/Checkbox"
+import ZodiacPaper from "./components/Paper"
+import ZodiacModal from "./components/Modal"
+import ZodiacTextField from "./components/input/TextField"
+import { theme as gnosisStyledComponentsTheme } from "@gnosis.pm/safe-react-components" // must expose here, to make sure ita compilable with the current version of the zodiac-ui-components
 
-export { Button, Checkbox, colors, doubleBorder, Modal, Paper, TextField, zodiacTheme }
+export {
+  ZodiacCheckbox,
+  colors,
+  doubleBorder,
+  ZodiacModal,
+  ZodiacPaper,
+  ZodiacTextField,
+  zodiacMuiTheme,
+  gnosisStyledComponentsTheme,
+}
