ee9ffa46daebfd98d85fe9a8365e94417926e0e1
Colin Spence <udappia@gmail.com>
2022-11-07 13:08:35 -0700

    Adds sliderwithinput

--- src/components/input/Slider/Slider.stories.tsx
+++ src/components/input/Slider/Slider.stories.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 import { Meta } from "@storybook/react/types-6-0"
 import Slider from "./Slider"
-import Input from "../Input"
-import { Box, Grid, SliderProps, Typography } from "@material-ui/core"
+import { Box, SliderProps } from "@material-ui/core"
 
 export default {
   title: "Components/Input/Slider",
@@ -16,50 +15,9 @@ export const SimpleSlider = (): SliderProps => (
 )
 
 export const SliderWithInput = (): SliderProps => {
-  const [value, setValue] = useState<number | string | Array<number | string>>(4)
-
-  const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue)
-  }
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value === "" ? "" : Number(event.target.value))
-  }
-
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if ((event.key === "ArrowUp" || event.key === "ArrowRight") && typeof value == "number") {
-      setValue(Number(value) + 1)
-    }
-    if ((event.key === "ArrowDown" || event.key === "ArrowLeft") && typeof value == "number") {
-      setValue(Number(value) - 1)
-    }
-  }
-
-  const handleBlur = () => {
-    if (value < 0) {
-      setValue(0)
-    } else if (value > 100) {
-      setValue(100)
-    }
-  }
-
   return (
-    <Box sx={{ width: 250 }}>
-      <Typography id="slider-with-input" gutterBottom>
-        Label
-      </Typography>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={9}>
-          <Slider
-            value={typeof value === "number" ? value : 0}
-            onChange={handleSliderChange}
-            aria-labelledby="slider-with-input"
-          />
-        </Grid>
-        <Grid item xs={3}>
-          <Input value={value} onChange={handleInputChange} onBlur={handleBlur} onKeyDown={handleKeyDown} />
-        </Grid>
-      </Grid>
+    <Box sx={{ width: 400 }}>
+      <Slider label="Slider label" id="slider" hasInput />
     </Box>
   )
 }
--- src/components/input/Slider/Slider.tsx
+++ src/components/input/Slider/Slider.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react"
-import { Slider as MUISlider, SliderProps, makeStyles } from "@material-ui/core"
+import React, { useState } from "react"
+import { Grid, Slider as MUISlider, SliderProps as MUISliderProps, makeStyles, Typography } from "@material-ui/core"
+import TextField from "../TextField"
 import { colors } from "../../../theme/zodiacTheme"
+import { MaximizeSharp } from "@material-ui/icons"
 
-export type { SliderProps }
+interface SliderProps extends MUISliderProps {
+  hasInput?: boolean
+  id?: string
+  label?: string
+}
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -23,11 +29,84 @@ const useStyles = makeStyles(() => ({
       backgroundColor: colors.tan[1000],
     },
   },
+  sliderGrid: {
+    flexGrow: 1,
+  },
+  inputGrid: {
+    width: 80,
+  },
+  input: {
+    border: `1px solid ${colors.tan[300]}`,
+    textAlign: "center",
+  },
 }))
 
-const Slider = ({ ...props }: SliderProps) => {
+const Slider = ({ hasInput, id, label, ...props }: SliderProps) => {
   const classes = useStyles()
 
+  const min = props.min || 0
+  const max = props.max || 100
+
+  if (hasInput) {
+    const [value, setValue] = useState<number | string | Array<number | string>>(4)
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+      setValue(newValue)
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value === "" ? "" : Number(event.target.value))
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ((event.key === "ArrowUp" || event.key === "ArrowRight") && typeof value == "number") {
+        if (value < max) {
+          setValue(Number(value) + 1)
+        }
+      }
+      if ((event.key === "ArrowDown" || event.key === "ArrowLeft") && typeof value == "number") {
+        if (value > min) {
+          setValue(Number(value) - 1)
+        }
+      }
+    }
+
+    const handleBlur = () => {
+      if (value < min) {
+        setValue(min)
+      } else if (value > max) {
+        setValue(max)
+      }
+    }
+    return (
+      <>
+        {label && (
+          <Typography id={id} gutterBottom>
+            {label}
+          </Typography>
+        )}
+        <Grid container spacing={3} alignItems="center">
+          <Grid item className={classes.sliderGrid}>
+            <Slider
+              value={typeof value === "number" ? value : 0}
+              onChange={handleSliderChange}
+              aria-labelledby={id}
+              {...props}
+            />
+          </Grid>
+          <Grid item className={classes.inputGrid}>
+            <TextField
+              InputProps={{ className: classes.input }}
+              value={value}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
+            />
+          </Grid>
+        </Grid>
+      </>
+    )
+  }
   return <MUISlider className={classes.root} {...props} />
 }
 
